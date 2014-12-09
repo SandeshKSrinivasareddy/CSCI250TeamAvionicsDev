b52a54c4263d71d7484d2870ee23d9cab23f02f3
SandeshKSrinivasareddy <sandeshks@mail.fresnostate.edu>
2014-12-08 21:37:58 -0800

    Team Check-in 1.1
    Team Check-in

---
+++ Radio1.1.java
@@ -0,0 +1,26 @@
+/**
+ * Simulated Radio class.
+ */
+import java.util.Random;
+
+public class Radio {
+	int seed;
+	int badCounter = 5;
+	int ID1 = seed + 767;
+	int ID2 = seed + 321;
+	double startRad = 0;
+	public Radio(int seed){
+		this.seed = seed;
+	}
+	public double getRadial(){
+		double radial;
+		Random dice = new Random();
+		radial = dice.nextDouble() * 3.45;
+		int tick = dice.nextInt(2);
+		if(tick > 0){
+			return startRad + radial;
+		}
+		return startRad + 360 - radial;
+	}
+	
+}
